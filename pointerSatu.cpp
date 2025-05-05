024d55c565ba45c27888047694d66568c12e8a45
AhmadMizan038 <ahmdmzn01@gmail.com>
2025-05-05 10:58:29 +0700

    3

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -8,3 +8,13 @@ void shownim(){
     cout <<"No Induk =" <<nim<<endl;
 }
 };
+
+int main(){
+
+    mahasiswa mhs{1};
+    mhs.shownim();
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.shownim();
+}
